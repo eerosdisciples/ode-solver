bf8b09005e72e5116a45c567ffa78310021578b4
hoppe93 <mathias.hoppe2060@gmail.com>
2015-02-13 11:06:56 +0100

    Re-wrote "main.c" as a test program for matrix operations

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 /* Test program */
 
 #include <stdio.h>
+#include "include/matrix.h"
 
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+void printm(matrix m) {
+	printf("[%f %f %f]\n", m.val[0][0], m.val[1][0], m.val[2][0]);
+}
+int main(){
+	matrix a = mnew(3, 1), b = mnew(3, 1);
+	a.val[0][0] = 1, a.val[1][0] = 2, a.val[2][0] = 3;
+	b.val[0][0] = 1, b.val[1][0] = 2, b.val[2][0] = 3;
+	
+	matrix m = madd(a,b);
+	
+	printm(m);
 	return 0;
 }
