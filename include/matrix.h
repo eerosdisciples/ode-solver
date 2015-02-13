fe991168d462909963119c82204446c79572b6d4
hoppe93 <mathias.hoppe2060@gmail.com>
2015-02-13 10:41:06 +0100

    Added matrix operations skeleton and include dir

---
+++ include/matrix.h
@@ -0,0 +1,11 @@
+#ifndef _MATRIX_H
+#define _MATRIX_H
+
+/* Define the matrix type */
+typedef struct {
+	double **data;		/* The actual matrix */
+	unsigned int m;		/* Number of rows */
+	unsigned int n;		/* Number of columns */
+} matrix;
+
+#endif/*_MATRIX_H*/
---
+++ matrix.c
@@ -0,0 +1,30 @@
+/* Matrix operations */
+
+#include "include/matrix.h"
+
+/*
+ * Create a new m-by-n matrix
+ *
+ * m: Number of rows
+ * n: Number of columns
+ * RETURNS: A new m-by-n matrix
+ */
+matrix mnew(unsigned int m, unsigned int n) {}
+/* 
+ * Add two matrices
+ *
+ * a: First matrix to add
+ * b: Second matrix to add
+ * RETURNS: A new matrix with
+ * elements equal to a+b
+ */ 
+matrix madd(matrix a, matrix b) {}
+
+/*
+ * Multiply each element of a matrix
+ * by a scalar.
+ *
+ * scalar: Scalar to multiply with
+ * a: Matrix to multiply with
+ */
+matrix mmuls(double scalar, matrix a) {}
