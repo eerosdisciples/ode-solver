fc5ebc58119e2744252c382737a8ab01a210a50f
Mathias Hoppe <mathias.hoppe2060@gmail.com>
2015-02-13 16:20:49 +0100

    Fixed a typo in "main.c"

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ void printm(matrix m) {
 	printf("[%f %f %f]\n", m.val[0][0], m.val[1][0], m.val[2][0]);
 }
 int main(){
-	matrix a, b;
+	matrix a = mnew(3,1), b = mnew(3,1);
 	a.val[0][0] = 1, a.val[1][0] = 2, a.val[2][0] = 3;
 	b.val[0][0] = 1, b.val[1][0] = 2, b.val[2][0] = 3;
 	
