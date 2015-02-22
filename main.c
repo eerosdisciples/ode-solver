ff746721969cf26fa0422a5e680572ce8ad1c8ee
Aylwini <johndoe@example.com>
2015-02-22 17:52:41 +0100

    main to test euler

--- main.c
+++ main.c
@@ -24,17 +24,3 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-	/* Define matrix with arbitrary cartesian coords */
-/*	vector carcoord=vinit(3,1.0,2.0,3.0);
-	vector ccord=findcoords(carcoord);
-		printf("r is %f, phi is %f,z is %f\n",ccord.val[0],ccord.val[1],ccord.val[2]);
-	
-	
-		double *B_phi=load_magnetic_field(argv[1]);
-		printf("first value in Bphi is %f\n",B_phi[1]);//	double Br[]=
-	
-	
-			vector B=transform(carcoord ,B_phi,B_phi,B_phi);
-
-			printf("Bx is %f, By is %f,Bz is %f\n",B.val[0],B.val[1],B.val[2]);
-	*/
