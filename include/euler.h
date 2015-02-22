dec3c81be0cb49bad241d2396a6cbc9181938fed
Aylwini <johndoe@example.com>
2015-02-22 17:46:38 +0100

    Added euler.h + changes to magnetic_field.h

---
+++ include/euler.h
@@ -0,0 +1,11 @@
+#ifndef _EULER_H
+#define _EULER_H
+
+vector findcoords(vector);
+vector transform(vector ,double* ,double* ,double*);
+double inter(vector,double*);
+void eulermethod(int ,int ,vector ,double* ,double* ,double* );
+
+
+
+#endif/*_EULER_H*/
---
+++ include/magnetic_field.h
@@ -0,0 +1,6 @@
+#ifndef _MAGNETIC_FIELD_H
+#define _MAGNETIC_FIELD_H
+
+double * load_magnetic_field(char*,int);
+
+#endif/*_MAGNETIC_FIELD_H*/
