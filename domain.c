b834e5b4384d95f7be705a7fba1d46f5157f3b5a
Aylwini <johndoe@example.com>
2015-02-24 11:43:29 +0100

    Updated domain functions

--- domain.c
+++ domain.c
@@ -28,7 +28,47 @@ domain *domain_load(char *filename) {
  * is inside, on the border of or outside the given
  * domain
  */
-int domain_check(domain *d, double r, double z) {
+int domain_check(domain *d, double *r, double* z) { 
+		double det;/* Determinant */
+	
+	/* Variables used in method */
+	double x00=r[0];
+	double x10=r[1];
+	double y00=z[0];
+	double y10=z[1];
+	double x01;
+	double y01;
+	double x11;
+	double y11;
+	double s;
+	double t;
+	/* Used in loop */  
+	int i;
+	
+	/* Check if matrix is zero */
+	if (x00-x10==0 && y00-y01==0)
+		return DOMAIN_OUTSIDE;
+
+	for (i=0;i<d->n;i++){
+		x01=d->r[i];
+		x11=d->r[i+1];
+		y01=d->z[i];
+		y11=d->z[i+1];
+		
+		/* Calculates the determinant */
+		det=x11*y01-x01*y11;
+		
+		/* Calculates s and t */
+		
+		s=1/det*((x00-x10)*y01-(y00-y10)*x01);
+		t=1/det*(-(-x00+x10)*y11+(y00-y10)*x11);
+		
+		/* If s and t are between 0 and 1 => intersection */
+		if (s>=0 && s<=1 && t>=0 && t<=1)
+			return DOMAIN_OUTSIDE;
+		
+	}
+		
 	return DOMAIN_WITHIN;
 }
 
