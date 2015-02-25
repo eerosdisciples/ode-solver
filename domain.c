e0c5ac615dad8dc7a98b8925097e92ca592b4fdc
Aylwini <johndoe@example.com>
2015-02-25 17:35:08 +0100

    Modified domain_check to notice whether a point is outside or inside the contour

--- domain.c
+++ domain.c
@@ -79,28 +79,27 @@ domain *domain_load(char *filename) {
  * on the border of the domain.
  *
  * d: Domain for the problem
- * r: Radial coordinate
- * z: Z-coordinate
+ * r: next Radial coordinate
+ * z: next Z-coordinate
  *
  * RETURNS one of DOMAIN_WITHIN, DOMAIN_ONDOMAIN or
  * DOMAIN_OUTSIDE depending on wether the given point
- * is inside, on the border of or outside the given
+ * is inside or outside the given
  * domain
  */
-int domain_check(domain *d, double *r, double* z) { 
-	/*Råkade blanda ihop linjerna är av formen uo+tv0=(x00,y00)+t(x01,y01) */
+int domain_check(domain *d, double r, double z) { 
 	
 	/* Determinant */
 	double det;
-	//printf("Just a check, z[1] is %f and z[2] is %f\n",z[0],z[1]);
-	//printf("Just a check, r[1] is %f and r[2] is %f\nma",r[0],r[1]);
 	
-	/* Parametrisation*/
+	/* Parametrization*/
 	
-	double x00=r[0];
-	double x01=r[1]-x00;
-	double y00=z[0];
-	double y01=z[1]-y00;
+	/* A point inside the conotur */
+	double x00=6.5;
+	double y00=2.3;
+	
+	double x01=r-x00;
+	double y01=z-y00;
 	
 	double x10;
 	double y10;
@@ -110,26 +109,24 @@ int domain_check(domain *d, double *r, double* z) {
 	double t;
 	/* Used in loop */  
 	int i;
-	//printf("x00 is %f x10 is %f y00 is %f y10 is %f \n",x00,x10,y00,y10);
+	
+	/* Variable for counting each intersection */
+	int count=0;
 	
 	/* Check if matrix is zero */
 	if (x00-x10==0 && y00-y10==0)
-		return DOMAIN_OUTSIDE;
+		count++;
+		//return DOMAIN_OUTSIDE;
 
 	for (i=0;i<d->n;i++){
 		x10=d->r[i];
 		x11=d->r[i+1]-x10;
 		y10=d->z[i];
 		y11=d->z[i+1]-y10;
-		
-		//printf("x01 is %f x11 is %f y01 is %f y11 is %f \n",x01,x11,y01,y11);
-		
-		
+				
 		/* Calculates the determinant */
 		det=x11*y01-x01*y11;
-		
-	//	printf("Determinant is %f\n",det);
-		
+				
 		/* Check if determinant is zero */
 		if (det==0)
 		return DOMAIN_WITHIN;
@@ -137,14 +134,16 @@ int domain_check(domain *d, double *r, double* z) {
 		/* Calculates s and t */
 		s=(1/det)*((x00-x10)*y01-(y00-y10)*x01);
 		t=(1/det)*(-(-(x00-x10)*y11+(y00-y10)*x11));
-		
-		//printf("s is %f and t is %f\n",s,t);
-		
+				
 		/* If s and t are between 0 and 1 => intersection */
 		if (s>=0 && s<=1 && t>=0 && t<=1)
-			return DOMAIN_OUTSIDE;
+			count++;
+			//return DOMAIN_OUTSIDE;
 		
 	}
+	/* If nbr of intersections is odd, then the point is outside */
+if (count % 2)
+	return DOMAIN_OUTSIDE;
 		
 	return DOMAIN_WITHIN;
 }
