ae1a3ef8f640faa939d3d34c2e68f3a18d2367c4
Aylwini <johndoe@example.com>
2015-02-26 16:59:58 +0100

    some changes

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 /* The main program */
-
+#include "readfile.h"
+#include "domain.h"
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {// argc- hur många argument och argv själva filen
+	 domain *d=domain_load(argv[1]);
+		 double r[2]={5.39718,5.39718};
+	 double z[2]={-3.5,-1};
+	 
+int inside = domain_check(d, r, z);
+
+ 
+if (inside==1)
+printf("Not in Domain \n");
+
+if (inside==0)
+printf("In Domain\n");
+	 // domain_test();
+
 	return 0;
 }
