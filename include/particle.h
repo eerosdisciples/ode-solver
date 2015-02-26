4b2bf1ce88f34d77de9d97650dcb27c25c82a370
Ingrid <ingstra@student.chalmers.se>
2015-02-26 15:45:59 +0100

    switched to pointers in functions

--- include/particle.h
+++ include/particle.h
@@ -13,8 +13,16 @@ typedef struct {
   double val[6];		/* The particle vector */
 } particle;
 
-particle particle_new();
-particle particle_init(double, double, double, double, double, double);
-particle particle_add(particle, particle);
+particle* particle_new();
+particle* particle_init(double, double, double, double, double, double);
+particle* particle_add(particle, particle);
+
+double particle_get_position_x(particle*);
+double particle_get_position_y(particle*);
+double particle_get_position_z(particle*);
+
+double particle_get_velocity_x(particle*);
+double particle_get_velocity_y(particle*);
+double particle_get_velocity_z(particle*);
 
 #endif/*_PARTICLE_H*/
--- particle.c
+++ particle.c
@@ -6,29 +6,30 @@
 
 /*
  * Create a new particle
- * RETURNS: a generic particle
+ * RETURNS: pointer to a generic particle
  */
-particle particle_new() {
-	/* Declare six dimensional particle vector */
-	particle p[6];
+particle* particle_new() {
+  /* Declare pointer to particle and allocate memory */
+  particle *p;
+  p = (particle*)malloc(sizeof(particle));
 
-	return particle p;
+  return p;
 }
 /*
  * Initialize a new particle with specified position and velocity
- * RETURNS: The new particle
+ * RETURNS: Pointer to the new particle
  */
 
-particle particle_init(double x, double y, double z, double v_x, double v_y, double v_z ) { 
-  /* Declare particle */
-  particle p[6];
+particle* particle_init(double x, double y, double z, double v_x, double v_y, double v_z ) { 
+  /* Declare pointer to particle */
+  particle *p = particle_new();
 
-  p[0] = x;
-  p[0] = y;
-  p[0] = z;
-  p[0] = v_x;
-  p[0] = v_y;
-  p[0] = v_z;
+  p->val[0] = x;
+  p->val[1] = y;
+  p->val[2] = z;
+  p->val[3] = v_x;
+  p->val[4] = v_y;
+  p->val[5] = v_z;
 
   return p;
 }
@@ -37,21 +38,93 @@ particle particle_init(double x, double y, double z, double v_x, double v_y, dou
 /* 
  * Add two particles
  *
- * a: First particle to add
- * b: Second particle to add
- * RETURNS: A new particle with
+ * a: Pointer to first particle to add
+ * b: Pointer to second particle to add
+ * RETURNS: Pointer to a new particle with
  * elements equal to a+b
  */ 
-particle particle_add(particle a, particle b) {
+particle* particle_add(particle *a, particle *b) {
 	
-	/* Declare new particle */
-	particle p= newParticle();
+	/* Declare pointer to new particle */
+	particle *p = particle_new();
 
 	/* Sum over all elements */
 	unsigned int i;
 	for (i = 0; i < 6; i++) {
-		p.val[i] = a.val[i] + b.val[i];
+		p->val[i] = a->val[i] + b->val[i];
 	}
 
 	return p;
+}
+
+/*
+ * Get value of particle's x-coordinate position
+ *
+ * a: Pointer to particle
+ *
+ * RETURNS: particle's x-coordinate position
+ */
+
+double particle_get_position_x(particle *a) {
+  return a->val[0];
+}
+
+/*
+ * Get value of particle's y-coordinate position
+ *
+ * a: Pointer to particle
+ *
+ * RETURNS: particle's y-coordinate position
+ */
+
+double particle_get_position_y(particle *a) {
+  return a->val[1];
+}
+
+/*
+ * Get value of particle's z-coordinate position
+ *
+ * a: Pointer to particle
+ *
+ * RETURNS: particle's z-coordinate position
+ */
+
+double particle_get_position_z(particle *a) {
+  return a->val[2];
+}
+
+/*
+ * Get value of particle's x-coordinate velocity
+ *
+ * a: Pointer to particle
+ *
+ * RETURNS: particle's x-coordinate velocity
+ */
+
+double particle_get_velocity_x(particle *a) {
+  return a->val[3];
+}
+
+/*
+ * Get value of particle's y-coordinate velocity
+ *
+ * a: Pointer to particle
+ *
+ * RETURNS: particle's y-coordinate velocity
+ */
+
+double particle_get_velocity_y(particle *a) {
+  return a->val[4];
+}
+
+/*
+ * Get value of particle's z-coordinate velocity
+ *
+ * a: Pointer to particle
+ *
+ * RETURNS: particle's z-coordinate velocity
+ */
+
+double particle_get_velocity_z(particle *a) {
+  return a->val[5];
 }
