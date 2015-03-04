1c2fd3d685d8fc17264699b59289da73d213f603
Aylwini <johndoe@example.com>
2015-03-04 09:18:38 +0100

    added functions and method for RK,not completely done yet

--- include/ode.h
+++ include/ode.h
@@ -1,19 +1,29 @@
 #ifndef _ODE_H
 #define _ODE_H
 
+#define REDO_STEP 1
+#define OK_STEP 0
+
 #include "vector.h"
 
 typedef struct {
-	double *t;		/* Time points */
-	vector *z;		/* Solution points, z = z(t) */
-	unsigned int n;	/* Number of points in solution */
+	vector * Z;    /* Solution points, z = z(t) */
+	double step;	/* Optimal step */
+	int flag;	   /* If iteration should be redone */
 } ode_solution;
 
+//	unsigned int n;	/* Number of points in solution */
+
 /* Solve an initial value problem */
-vector *ode_solve(
+/*vector *ode_solve(
 	vector *(equation)(double, vector*),
 	double, vector*, unsigned int
-);
+);*/
+
+vector * ode_step(vector *(equation)(double, vector*),ode_solution *parameters, double T,int order);
+ode_solution* ode_solve( vector *(equation)(double, vector*),ode_solution *parameters,double T);
+
+
 
 void ode_test(void);
 
