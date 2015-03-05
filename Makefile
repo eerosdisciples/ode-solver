d6e4d03c1e7b525c89dd3dcedf602393ff45b7a6
Aylwini <johndoe@example.com>
2015-03-05 13:18:53 +0100

    modifications

--- Makefile
+++ Makefile
@@ -4,10 +4,8 @@ CC=gcc
 CFLAGS=-Wall -Wextra -g -O0 -c -I include
 LD=gcc
 
-LFLAGS=/usr/local/lib/libinterp2d.a -lgsl -lgslcblas
-
 TARGET=solver
-OBJECTS=arguments.o ctsv.o domain.o equation.o magnetic_field.o main.o ode.o readfile.o vector.o interp2.o
+OBJECTS=arguments.o ctsv.o equation.o main.o ode.o readfile.o vector.o
 
 all: $(TARGET)
 
--- main.c
+++ main.c
@@ -2,25 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "ode.h"
+
 
 #include "arguments.h"
 #include "domain.h"
-#include "magnetic_field.h"
 #include "readfile.h"
 
 int main(int argc, char *argv[]) {
-	arguments *args;
-	domain *dom;
-	magnetic_field *mf;
-
-	args = parse_args(argc, argv);
-
-	/* Load domain */
-	dom = domain_load(args->domain_file);
-	/* Load magnetic field */
-	mf  = magnetic_field_load(args->magfield_file);
 
 	/* Solve */
-
+	ode_test();
 	return 0;
 }
