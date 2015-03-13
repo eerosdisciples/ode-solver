10bc19c3e432dcd7e5a0ca8b0218750e35805d3f
Aylwini <aylwin_i@yahoo.se>
2015-03-13 12:52:26 +0100

    Update ode.h

--- include/ode.h
+++ include/ode.h
@@ -17,9 +17,10 @@ typedef struct {
 	double* T; 		/* Time points */
 	double* E;  		/* Energy */
 	vector *v;		/* List of coordinates to output */
-	char **labels;			/* List of labels belonging to vectors `v' */
+	char **labels;			/* List of labels */
 	unsigned int points;	/* Number of points */
 	unsigned int nvars;		/* Number of coordinate-variables (or columns) */
+
 } solution_data;
 
 
