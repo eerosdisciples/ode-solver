a44ce4ec115eee12266e9c22250aa64e27fa9f67
Aylwini <aylwin_i@yahoo.se>
2015-03-13 13:00:17 +0100

    Update ode.h
    Removed solution_data object

--- include/ode.h
+++ include/ode.h
@@ -13,16 +13,6 @@ typedef struct {
 } ode_solution;
 
 
-typedef struct {
-	double* T; 		/* Time points */
-	double* E;  		/* Energy */
-	vector *v;		/* List of coordinates to output */
-	char **labels;			/* List of labels */
-	unsigned int points;	/* Number of points */
-	unsigned int nvars;		/* Number of coordinate-variables (or columns) */
-
-} solution_data;
-
 
 
 /* Solve an initial value problem */
