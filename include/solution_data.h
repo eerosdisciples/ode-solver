d591236d1d8bed207ceea059d45c6e86fb930297
Aylwini <johndoe@example.com>
2015-03-15 14:04:39 +0100

    Divided main into two functions

--- include/solution_data.h
+++ include/solution_data.h
@@ -17,6 +17,17 @@ typedef struct {
   unsigned int nvars;		// Number of coordinate-variables (or columns) 
 } solution_data;
 
+typedef struct {
+  double t0;     	     // starting time
+  double tmax;           // ending time
+  double x0;			// Starting positions
+  double y0;
+  double z0;
+  double vx0;          // Starting velocity
+  double vy0;
+  double vz0;
+} initial_data;
+
 
 
 #endif/*_SOLUTION_DATA_H*/
--- main.c
+++ main.c
@@ -16,24 +16,134 @@
 
 #define NUMBER_OF_POINTS 1000 // number of data points to start with
 /* Reference point to check if initial position is inside domain */
-#define REFERENCE_POINT_X 6 
-#define REFERENCE_POINT_Y 0 
+#define REFERENCE_POINT_R 6 
+#define REFERENCE_POINT_Z 0 
+
+/**
+ * Calculates saves the values of the particle motion with given parameters
+ * Uses ctsv_write
+ * solution: Vector with particle parameters and initial values to function giving the equation,
+ */
+
+void main_solve(domain *dom,initial_data initial, particle *part){
+    unsigned int i;
+    double vx,vy,vz;
+    vector *solution;
+    ode_solution *solver_object;
+	
+  
+    unsigned int points;
+    points = NUMBER_OF_POINTS;
+	
+    /* Allocate memory for solution vector */
+    solution = malloc(sizeof(vector)*points);
+    /* solver_object of type ode_solution contains solution points, optimal
+     * step size, and flag indicating ok step. */
+    solver_object = malloc(sizeof(ode_solution));
+    solver_object->step = 1e-10; /* Initial step size */
+		
+    solution->n = 6;
+    solution->val = malloc(sizeof(double)*6);
+    solution->val[0] = initial.x0; 
+    solution->val[1] = initial.y0;
+    solution->val[2] = initial.z0; 
+    solution->val[3] = initial.vx0;
+    solution->val[4] = initial.vy0;
+    solution->val[5] = initial.vz0;
+	
+    /* For storing time */
+    double *t = malloc(sizeof(double)*(points+1));
+	t[0]=initial.t0;
+    /* For storing energy */
+    double *E = malloc(sizeof(double)*(points+1));
+    E[0] = part->mass/2*(vx*vx + vy*vy + vz*vz);
+
+    /* For checking domain */
+    double x,y,z,r;
+
+    x = solution->val[0];
+    y = solution->val[1];
+    z = solution->val[2];
+    r = sqrt(x*x + y*y);
+  
+    double R[2] = {REFERENCE_POINT_R,r};
+    double Z[2] = {REFERENCE_POINT_Z,z};
+
+    if (domain_check(dom, R, Z) == DOMAIN_OUTSIDE) {
+      printf("Particle starts outside reactor!\n");
+      exit(EXIT_FAILURE);
+    }
+
+    i = 0;
+    while (t[i] < initial.tmax) {
+      /* Check if we have enough points */
+      if (i+2 >= points) {
+        unsigned int np = (unsigned int)ceil(points * initial.tmax/t[i]);
+        solution = realloc(solution, np*(sizeof(vector)));
+        t = realloc(t, np*sizeof(double));
+        E = realloc(E, np*sizeof(double));
+
+        points = np;
+      }
+
+      solver_object->Z = solution+i;
+      do {
+        t[i+1] = t[i] + solver_object->step;
+        ode_solve(equation_particle, solver_object, t[i]);
+      } while (solver_object->flag == REDO_STEP);
+
+      x = solution[i].val[0];
+      y = solution[i].val[1];
+      z = solution[i].val[2];
+      vx= solution[i].val[0];
+      vy= solution[i].val[1];
+      vz= solution[i].val[2];
+      r = sqrt(x*x + y*y);
+      E[i+1] = part->mass/2 * (vx*vx + vy*vy + vz*vz);
+
+      /* Move on to next iteration */
+      i++;
+
+      R[0] = R[1]; Z[0] = Z[1];
+      R[1] = r;    Z[1] = z;
+      if (domain_check(dom, R, Z) == DOMAIN_OUTSIDE) {
+        printf("Particle collided with reactor wall!\n");
+        break;
+      }
+    }
+
+    printf("Number of points: %d\n", i);
+
+    /* Output data */
+    solution_data output;
+    output.T=t;
+    output.E=E;
+    output.v=solution;
+    output.labels=malloc(sizeof(char *)*6);
+    output.labels[0]="x";
+    output.labels[1]="y";
+    output.labels[2]="z";
+    output.labels[3]="vx";
+    output.labels[4]="vy";
+    output.labels[5]="vz";
+    output.points=i;
+    output.nvars=6;
+	
+    ctsv_write("particle.csv",',',&output);
+	
+}
+
 
 int main(int argc, char *argv[]) {
   /* Variable declarations */
   arguments *args;
   domain *dom;
   magnetic_field *B;
-  vector *solution;
-  ode_solution *solver_object;
   particle *part;
   args = parse_args(argc, argv);
+  initial_data initial;
   double vx,vy,vz;
-  unsigned int i;
-  
-  unsigned int points;
-  points = NUMBER_OF_POINTS;
-
+ 
   /* Load domain */
   dom = domain_load(args->domain_file);
   /* Load magnetic field */
@@ -47,106 +157,26 @@ int main(int argc, char *argv[]) {
   interp2_init_interpolation(B);
   equation_init(part);
 
-  /* Allocate memory for solution vector */
-  solution = malloc(sizeof(vector)*points);
-  /* solver_object of type ode_solution contains solution points, optimal
-   * step size, and flag indicating ok step. */
-  solver_object = malloc(sizeof(ode_solution));
-  solver_object->step = 1e-10; /* Initial step size */
-
   /* initial velocity from input file pi. TODO: Load input data to particle object, pass particle to solver function. Edit the particle type first. */
   vx = args->v0[0];
   vy = args->v0[1];
   vz = args->v0[2];
 
   /* Set initial values */
-  solution->n = 6;
-  solution->val = malloc(sizeof(double)*6);
-  solution->val[0] = args->r0[0];
-  solution->val[1] = args->r0[1];
-  solution->val[2] = args->r0[2];
-  solution->val[3] = vx;
-  solution->val[4] = vy;
-  solution->val[5] = vz;
-
-  /* For storing time */
-  double *t = malloc(sizeof(double)*(points+1));
-  t[0] = args->tstart;
-  /* For storing energy */
-  double *E = malloc(sizeof(double)*(points+1));
-  E[0] = part->mass/2*(vx*vx + vy*vy + vz*vz);
-
-  /* For checking domain */
-  double x,y,z,r;
-
-  x = solution->val[0];
-  y = solution->val[1];
-  z = solution->val[2];
-  r = sqrt(x*x + y*y);
+    
+  initial.x0 = args->r0[0];
+  initial.y0 = args->r0[1];
+  initial.z0 = args->r0[2];
   
-  double R[2] = {6,r};
-  double Z[2] = {0,z};
-
-  if (domain_check(dom, R, Z) == DOMAIN_OUTSIDE) {
-    printf("Particle starts outside reactor!\n");
-    exit(EXIT_FAILURE);
-  }
-
-  i = 0;
-  while (t[i] < args->tend) {
-    /* Check if we have enough points */
-    if (i+2 >= points) {
-      unsigned int np = (unsigned int)ceil(points * args->tend/t[i]);
-      solution = realloc(solution, np*(sizeof(vector)));
-      t = realloc(t, np*sizeof(double));
-      E = realloc(E, np*sizeof(double));
-
-      points = np;
-    }
-
-    solver_object->Z = solution+i;
-    do {
-      t[i+1] = t[i] + solver_object->step;
-      ode_solve(equation_particle, solver_object, t[i]);
-    } while (solver_object->flag == REDO_STEP);
-
-    x = solution[i].val[0];
-    y = solution[i].val[1];
-    z = solution[i].val[2];
-    vx= solution[i].val[0];
-    vy= solution[i].val[1];
-    vz= solution[i].val[2];
-    r = sqrt(x*x + y*y);
-    E[i+1] = part->mass/2 * (vx*vx + vy*vy + vz*vz);
-
-    /* Move on to next iteration */
-    i++;
-
-    R[0] = R[1]; Z[0] = Z[1];
-    R[1] = r;    Z[1] = z;
-    if (domain_check(dom, R, Z) == DOMAIN_OUTSIDE) {
-      printf("Particle collided with reactor wall!\n");
-      break;
-    }
-  }
-
-  printf("Number of points: %d\n", i);
-
-  /* Output data */
-  solution_data output;
-  output.T=t;
-  output.E=E;
-  output.v=solution;
-  output.labels=malloc(sizeof(char *)*6);
-  output.labels[0]="x";
-  output.labels[1]="y";
-  output.labels[2]="z";
-  output.labels[3]="vx";
-  output.labels[4]="vy";
-  output.labels[5]="vz";
-  output.points=i;
-  output.nvars=6;
-  ctsv_write("particle.csv",',',&output);
+  initial.vx0 = vx;
+  initial.vy0 = vy;
+  initial.vz0 = vz;
+	  
+  initial.t0 = args->tstart;
+  initial.tmax= args->tend;
+	  
+  
+  main_solve(dom,initial,part);
 
   return 0;
 }
